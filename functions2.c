32e7af10aeac0fc035a19a32d81d6b900199014e
JoshLosh00 <142682414+JoshLosh00@users.noreply.github.com>
2026-05-12 15:51:57 +0100

    Update functions2.c

--- functions2.c
+++ functions2.c
@@ -28,35 +28,21 @@ void pvector_push(struct pvector *v, const struct fdinfo c){
         v->size++;
 }
 
-/*
-void push_bytes(struct vector *v, const char *s, size_t length){
-
-        size_t old = v->size;
-        v->size += length;
-
-        if (v->size > v->capacity){
-                v->capacity = v->size*2;
-                char *tmp = realloc(v->data, v->capacity);
-                if (!tmp){
-                        printf("malloc error\n");
-                        return;  
-                }
-                v->data = tmp;
-        }
-
-        memcpy(v->data + old, s, length);
+void freefdinfo(struct fdinfo *info){
+    close(info->poll.fd);
+    info->poll.fd = -1;
+    fclose(info->file);
+    free(info->inbuffer);
+    free(info->outbuffer);
 }
-*/
 
-/*
-void vector_string_termination(struct vector *v){
-        if (v->size == v->capacity){
-                v->capacity++;
-                realloc(v->data, v->capacity);
-        }
-        v->data[v->size] = '\0'; 
+void freefdinfo2(struct fdinfo *info){
+    close(info->poll.fd);
+    info->poll.fd = -1;
+    //fclose(info->file);
+    free(info->inbuffer);
+    free(info->outbuffer);
 }
-*/
 
 
 void pvector_free(struct pvector *v){
